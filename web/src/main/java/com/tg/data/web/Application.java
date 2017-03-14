31d33a900c9709b18304e46596b9b31d4ab772e6
Anthony Li <lizhen.dgut@gmail.com>
2017-03-14 22:39:06 +0800

    【新增】Spring Boot 启动类

---
+++ web/src/main/java/com/tg/data/web/Application.java
@@ -0,0 +1,16 @@
+package com.tg.data.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 李震 on 2017/3/14.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
